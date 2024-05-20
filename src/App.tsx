8eb1ca8b85d10c63e38a64217ae108b3e255b3e8
humbl3LilaS <sabishinekobebe@gmail.com>
2024-05-20 22:53:01 +0630

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header.tsx";
+
+function App() {
+
+
+    return (
+        <>
+            <Header/>
+            <main>
+                hello
+            </main>
+        </>
+    );
+}
+
+export default App;
